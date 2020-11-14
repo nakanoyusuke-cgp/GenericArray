839c592b0e773b21cc878dbf092f92ca97a126ed
Lycolet <yusukenakano16@gmail.com>
2020-11-14 10:58:46 +0900

    Initial commit

---
+++ GenericArray/GenericArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
